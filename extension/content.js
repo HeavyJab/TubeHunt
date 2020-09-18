8a47c31b7b3bad31b7dfac4cfecfcb9f0296c0ef
Vincent Lam <lamming0912@gmail.com>
2020-09-18 16:14:16 +0800

    added css styling

--- extension/content.js
+++ extension/content.js
@@ -1,46 +1,67 @@
-let first_load = true;
-
 (async () => {
-    const channelCard = document.createElement("div");
-    channelCard.innerHTML = `
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-gray-700 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
+  const res = await fetch(
+    "https://us-central1-tube-hunt.cloudfunctions.net/app/api/channels"
+  );
+  const channels = await res.json();
+
+//   order channels by submission time
+  
+
+  const channelCard = document.createElement("div");
+  channelCard.setAttribute("id", "channel-section")
+
+  channels.forEach((channel) => {
+    channelCard.innerHTML += `
+
+    <div id="channel-card">
+    <div id=header>
+        <a href="${'/channel/' + channel.channelId}" id='profile-link' target="_blank">
+            <div id=channel-profile> 
+                <img id="channel-profile" src="${channel.imgSrc}">
+                <h2>
+                    ${channel.title}
+                </h2>
+            </div>
+        </a>
+
+        <a href="https://www.reddit.com/r/TubeHunt/" target="_blank">
+            <span>r/TubeHunt</span>
+        </a>
     </div>
-    <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+
+    <div id="channel-desc">
+        ${channel.desc}
+    </div>
+
+    <div id="channel-footer"> 
+        <button></button>
     </div>
     </div>
         `;
+  });
 
-    const res = await fetch(
-    "https://us-central1-tube-hunt.cloudfunctions.net/app/api/channels"
-    );
-    const channels = await res.json();
-
-    const inject = () => {
-        let mainPage = document.querySelector("#contents");
-        mainPage.prepend(channelCard);
-        first_load = false;
+  const mo = new MutationObserver(() => {
+    console.log("in observer");
+    if (!document.contains(channelCard)) {
+      console.log("Changing");
+      inject();
     }
+  });
 
-    inject();
+  const inject = () => {
+    mo.disconnect();
+    let mainPage = document.querySelector("#contents");
+    mainPage.prepend(channelCard);
+    observe();
+  };
 
-    const body = document.getElementsByTagName("body")[0];
-    body.addEventListener("yt-navigate", function(event) {
-        console.log(`in event listener ${first_load}`)
-        if(first_load === false){
-            inject();
-        }
-    })
+  inject();
 
- 
+  function observe() {
+    mo.observe(document.body, { childList: true, subtree: true });
+  }
+
+  observe();
 })();
 
 console.log("loaded the script!");
--- server/functions/index.js
+++ server/functions/index.js
@@ -94,7 +94,7 @@ app.post("/api/channel/submit", (req, res) => {
 app.get("/api/channels", (req, res) => {
   (async () => {
     try {
-      const channelSnapshot = await db.collection("channels").get();
+      const channelSnapshot = await db.collection("channels").orderBy("dateSubmitted").get();
       const channels = [];
       channelSnapshot.forEach((doc) => {
         channels.push(doc.data());
