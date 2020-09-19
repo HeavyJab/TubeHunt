dd6cdef45eb3db261edc1f7133f935fc1fd6aa4a
dchicchon <danielchicchon@gmail.com>
2020-09-19 00:14:09 -0700

    added styling to popup.html in popup.css and sorted the channels by upVoteCount in content.js

--- extension/content.js
+++ extension/content.js
@@ -5,15 +5,22 @@
   const channels = await res.json();
 
   const channelCard = document.createElement("div");
-  channelCard.setAttribute("id", "channel-section")
+  channelCard.setAttribute("id", "channel-section");
+
+  channels.sort((a, b) => {
+    return b.upvotesCount - a.upvotesCount;
+  });
 
   channels.forEach((channel) => {
     channelCard.innerHTML += `
+  console.log(channels);
 
     <div id="channel-card">
     <div id="header">
         <div id=channel-title>
-            <a href="${'/channel/' + channel.channelId}" id='profile-link' target="_blank">
+            <a href="${
+              "/channel/" + channel.channelId
+            }" id='profile-link' target="_blank">
                 <div id=channel-profile> 
                     <img id="channel-profile" src="${channel.imgSrc}">
                     <h3>
@@ -42,19 +49,27 @@
     <div id="channel-footer"> 
         <div id="voting">
             <div>
-                <button id="upvote" class="voting" name=${channel.channelId}>👏</button>
-                <span id="upvotesCount-${channel.channelId}">${channel.upvotesCount}</span>
+                <button id="upvote" class="voting" name=${
+                  channel.channelId
+                }>👏</button>
+                <span id="upvotesCount-${channel.channelId}">${
+      channel.upvotesCount
+    }</span>
             </div>
-            <a href="https://www.youtube.com/channel/${channel.channelId}?sub_confirmation=1">
-            <button id="subscribe" class="voting" name=${channel.channelId}>🍿</button>
+            <a href="https://www.youtube.com/channel/${
+              channel.channelId
+            }?sub_confirmation=1">
+            <button id="subscribe" class="voting" name=${
+              channel.channelId
+            }>🍿</button>
             </a>
         </div>
     </div>
     </div>
         `;
   });
-//   <button id="downvote" name=${channel.channelId}>👎</button>
-//   Observe DOM mutation
+  //   <button id="downvote" name=${channel.channelId}>👎</button>
+  //   Observe DOM mutation
   const mo = new MutationObserver(() => {
     if (!document.contains(channelCard)) {
       console.log("Changing");
@@ -62,42 +77,45 @@
     }
   });
 
-// Inject content
-    (observe = () => {
-      mo.observe(document.body, { childList: true, subtree: true });
-    })()
-    
+  // Inject content
+  (observe = () => {
+    mo.observe(document.body, { childList: true, subtree: true });
+  })()(
     (inject = () => {
-        mo.disconnect();
+      mo.disconnect();
 
-        let mainPage = document.querySelector("#contents");
-        mainPage.prepend(channelCard);
-    
-        window.onclick = async (event) => {
-            const target = event.target;
-            if (target.matches("#upvote")) {
-                const channelId = document.activeElement.getAttribute("name");
+      let mainPage = document.querySelector("#contents");
+      mainPage.prepend(channelCard);
 
-                // change the view
-                let upvotesCount = document.getElementById(`upvotesCount-${channelId}`).innerText;
-                const upvotesElm = document.getElementById(`upvotesCount-${channelId}`);
-                upvotesCount = parseInt(upvotesCount) + 1
-                upvotesElm.innerHTML = upvotesCount;
+      window.onclick = async (event) => {
+        const target = event.target;
+        if (target.matches("#upvote")) {
+          const channelId = document.activeElement.getAttribute("name");
 
-                // change model
-                const res = await fetch(
-                `https://us-central1-tube-hunt.cloudfunctions.net/app/api/${channelId}/upvote`,
-                {
-                    method: "get",
-                }
-                );
-                console.log('Upvoted!')
+          // change the view
+          let upvotesCount = document.getElementById(
+            `upvotesCount-${channelId}`
+          ).innerText;
+          const upvotesElm = document.getElementById(
+            `upvotesCount-${channelId}`
+          );
+          upvotesCount = parseInt(upvotesCount) + 1;
+          upvotesElm.innerHTML = upvotesCount;
+
+          // change model
+          const res = await fetch(
+            `https://us-central1-tube-hunt.cloudfunctions.net/app/api/${channelId}/upvote`,
+            {
+              method: "get",
             }
-        };
+          );
+          console.log("Upvoted!");
+        }
+      };
 
-        observe();
-    })();
-
+      observe();
+    })
+  )();
 })();
 
 console.log("loaded the script!");
