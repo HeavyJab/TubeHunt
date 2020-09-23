f26a1da09496050f151de2889590fdd04a78cb5d
Vincent Lam <lamming0912@gmail.com>
2020-09-24 01:11:56 +0800

    cleaned up the code, intergrated show video feature but FUCKING youtube API only allows a small number of calls. Second most used search engine my ass

--- extension/content.js
+++ extension/content.js
@@ -11,8 +11,6 @@
     return b.upvotesCount - a.upvotesCount;
   });
 
-  
-
   channels.forEach((channel) => {
     channelCard.innerHTML += `
     <div id="channel-card">
@@ -45,11 +43,13 @@
                 <button id="upvote" class="voting" name=${
                   channel.channelId
                 }>👏</button>
-                <span id="upvotesCount-${channel.channelId}">${channel.upvotesCount}</span>
+                <span id="upvotesCount-${channel.channelId}">${
+      channel.upvotesCount
+    }</span>
             </div>
 
-            <button id="show" name="${channel.channelId}"> 
-                show videos!
+            <button id="show" class="voting" name="${channel.channelId}"> 
+                📺
             </button>
 
             <a href="https://www.youtube.com/channel/${
@@ -75,80 +75,124 @@
   //   </div>
 
   //   Observe DOM mutation
+
+  let showVideos = false;
+
+// Observer class to watch for DOM changes
   const mo = new MutationObserver(() => {
     if (!document.contains(channelCard)) {
       console.log("Changing");
       inject();
+      
+      if(showVideos) {
+        console.log(showVideos)
+        injectVideos(showVideos);
+      }
     }
   });
 
-  // Inject content
+  // observe changes  
   observe = () => {
     mo.observe(document.body, { childList: true, subtree: true });
-  }
+  };
+
+  let mainPage = document.querySelector("#contents");
   
-  (
-    (inject = () => {
+  // inject channel cards immediately invoked
+  (inject = () => {
       mo.disconnect();
 
-      let mainPage = document.querySelector("#contents");
       mainPage.prepend(channelCard);
-
-      window.onclick = async (event) => {
-        const target = event.target;
-        if (target.matches("#upvote")) {
-          const channelId = document.activeElement.getAttribute("name");
-
-          // change the view
-          let upvotesCount = document.getElementById(
-            `upvotesCount-${channelId}`
-          ).innerText;
-          const upvotesElm = document.getElementById(
-            `upvotesCount-${channelId}`
-          );
-          upvotesCount = parseInt(upvotesCount) + 1;
-          upvotesElm.innerHTML = upvotesCount;
-
-          // change model
-          const res = await fetch(
-            `https://us-central1-tube-hunt.cloudfunctions.net/app/api/${channelId}/upvote`,
-            {
-              method: "get",
-            }
-          );
-          console.log("Upvoted!");
-        }
-
-        // show videos
-        if(target.matches("#show")) {
-            console.log('clicked!!!!')
-            const channelId = document.activeElement.getAttribute("name");
-
-            const videos = document.createElement("div");
-            videos.setAttribute("id", `video-section`);
-            videos.setAttribute("name", `${channelId}`)
-            videos.innerHTML = `<h1>${channelId}</h1>`
-            
-            if (document.querySelector(`#video-section`)) {
-                console.log("contains channel-section")
-                if (document.getElementById("video-section").getAttribute('name') != channelId) {
-                  document.getElementById('video-section').remove()
-                  console.log(`fetch channel videos by id${channelId}`)
-                  console.log("set inner html")
-                  mainPage.firstChild.insertAdjacentElement('afterend', videos);
-                } else {
-                  document.getElementById('video-section').remove()
-                }
-            } else {
-              console.log(`fetch channel videos by id${channelId}`)
-              console.log("set inner html")
-              mainPage.firstChild.insertAdjacentElement('afterend', videos);            } 
-        }
-      };
-
       observe();
-    })
-  )();
+  })();
+
+  // inject videos if videos exists
+  injectVideos = (videos) => {
+    mo.disconnect();
+
+    mainPage.firstChild.insertAdjacentElement("afterend", videos);
+
+    observe();
+  }
+
+  // create a video section html
+  createVideosSection = async (channelId) => {
+    const videos = document.createElement("div");
+    videos.setAttribute("id", `video-section`);
+    videos.setAttribute("name", `${channelId}`);
+
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCUY5bnDpFKFrHh6btLHNt5MT6PHIZBSyk&channelId=${channelId}&part=snippet,id&order=date&maxResults=5`,
+      {
+        method: "get",
+      }
+    );
+
+    if (res.status == 200) {
+      data = await res.json();
+      data.items.forEach((video) => {
+        videos.innerHTML += `
+        <iframe width="250" height="170"
+        src="https://www.youtube.com/embed/${video.id.videoId}">
+        </iframe>
+      `;
+      });
+    } else {
+      console.log(res.status)
+    }
+
+    return videos;
+  };
+
+  // monitor all click events
+  window.onclick = async (event) => {
+    const target = event.target;
+    if (target.matches("#upvote")) {
+      const channelId = document.activeElement.getAttribute("name");
+
+      // change the view
+      let upvotesCount = document.getElementById(`upvotesCount-${channelId}`)
+        .innerText;
+      const upvotesElm = document.getElementById(`upvotesCount-${channelId}`);
+      upvotesCount = parseInt(upvotesCount) + 1;
+      upvotesElm.innerHTML = upvotesCount;
+
+      // change model
+      const res = await fetch(
+        `https://us-central1-tube-hunt.cloudfunctions.net/app/api/${channelId}/upvote`,
+        {
+          method: "get",
+        }
+      );
+      console.log("Upvoted!");
+    }
+
+    // show videos
+    if (target.matches("#show")) {
+      console.log("clicked!!!!");
+      const channelId = document.activeElement.getAttribute("name");
+
+      if (showVideos) {
+        console.log("contains channel-section");
+        if (
+          document.getElementById("video-section").getAttribute("name") != channelId
+        ) {
+          document.getElementById("video-section").remove();
+          const videos = await createVideosSection(channelId);
+          showVideos = videos
+          injectVideos(videos);
+        } else {
+          showVideos = false;
+          document.getElementById("video-section").remove();
+        }
+        // if there is no showVideos
+      } else {
+        const videos = await createVideosSection(channelId);
+        showVideos = videos
+        injectVideos(videos);
+      }
+    }
+  };
 })();
 
 console.log("loaded the script!");
