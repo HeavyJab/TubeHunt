bae7f3fff5eaa75a14dc3023f056b96449628e82
Rajesh Durai <rajesh.durai@rea-group.com>
2020-09-25 02:14:14 +1000

    Chrome-extension hot-loading wizardry???
    - Props to https://github.com/ceoimon/react-parcel-ts-extension-boilerplate.

---
+++ extension/src/extensionAPIs.js
@@ -0,0 +1,68 @@
+const apis = [
+  'alarms',
+  'bookmarks',
+  'browserAction',
+  'commands',
+  'contextMenus',
+  'cookies',
+  'downloads',
+  'events',
+  'extension',
+  'extensionTypes',
+  'history',
+  'i18n',
+  'idle',
+  'notifications',
+  'pageAction',
+  'runtime',
+  'storage',
+  'tabs',
+  'webNavigation',
+  'webRequest',
+  'windows',
+]
+
+class Extension {
+  constructor() {
+    for (const api of apis) {
+      this[api] = null
+
+      try {
+        if (chrome[api]) {
+          this[api] = chrome[api]
+          continue
+        }
+      } catch (_) {
+        //
+      }
+
+      try {
+        if (window[api]) {
+          this[api] = window[api]
+          continue
+        }
+      } catch (_) {
+        //
+      }
+
+      try {
+        if (browser[api]) {
+          this[api] = browser[api]
+          continue
+        }
+      } catch (_) {
+        //
+      }
+
+      try {
+        this[api] = browser.extension[api]
+      } catch (_) {
+        //
+      }
+    }
+  }
+}
+
+const extension = new Extension()
+
+export default extension
