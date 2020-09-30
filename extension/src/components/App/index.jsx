0fd6f2763e68057ead005a9cde06fd38fe9fe7c0
Rajesh Durai <rajesh.durai@rea-group.com>
2020-09-30 23:08:35 +1000

    Working feedback request straight to firestore.

--- extension/src/components/App/index.jsx
+++ extension/src/components/App/index.jsx
@@ -4,6 +4,14 @@ import SignInButton from '../SignInButton';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
+const submitFeedbackFn = feedbackString =>
+  firebase.firestore()
+    .doc('foo/bar')
+    .set({
+      feedbackMessage: feedbackString,
+      submissionTime: new firebase.firestore.Timestamp.now()
+    });
+
 const App = () => (
   <div>
     <SignInButton />
